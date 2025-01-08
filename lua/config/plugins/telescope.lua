c23728e7ea2d3656194d371ca0978ba248592c94
Carlos Orue <carlos@orue.io>
2025-01-07 19:37:53 -0600

    Update Telescope configuration

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -5,21 +5,58 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.5",
-    dependencies = { "nvim-lua/plenary.nvim" },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+        enabled = true
+      }, { "nvim-telescope/telescope-file-browser.nvim", enabled = true }
+
+    },
+
+
     config = function()
       require("telescope").setup({
+        defaults = {
+          sorting_strategy = "ascending",
+          layout_strategy = "horizontal",
+          layout_config = { prompt_position = "top" },
+        },
+
         extensions = {
           ["ui-select"] = {
             require("telescope.themes").get_dropdown({}),
           },
+          file_browser = {
+            path = "%:p:h",         -- open from within the folder of your current buffer
+            display_stat = false,   -- don't show file stat
+            grouped = true,         -- group initial sorting by directories and then files
+            hidden = true,          -- show hidden files
+            hide_parent_dir = true, -- hide `../` in the file browser
+            hijack_netrw = true,    -- use telescope file browser when opening directory paths
+            prompt_path = true,     -- show the current relative path from cwd as the prompt prefix
+            use_fd = true           -- use `fd` instead of plenary, make sure to install `fd`
+          }
         },
       })
-      local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-      vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
 
+
+      require("telescope").load_extension("file_browser")
+      require("telescope").load_extension("fzf")
       require("telescope").load_extension("ui-select")
-    end,
+
+
+      local builtin = require("telescope.builtin")
+
+      -- key maps
+
+      local opts = { noremap = true, silent = true }
+
+      vim.keymap.set("n", "-", ":Telescope file_browser<CR>")         -- Open the file browser
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, opts)     -- Lists files in your current working directory, respects .gitignore
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, opts)      -- Grep for a string in your current working directory
+      vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, opts) -- Open a list of your recently opened files
+    end
   },
 }
--- lua/config/plugins/treesitter.lua
+++ lua/config/plugins/treesitter.lua
@@ -2,8 +2,16 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
+    dependencies = {
+      { "nvim-treesitter/nvim-treesitter-textobjects" }, -- Syntax aware text-objects
+      {
+        "nvim-treesitter/nvim-treesitter-context",       -- Show code context
+        opts = { enable = true, mode = "topline", line_numbers = true }
+      }
+    },
     config = function()
       local config = require("nvim-treesitter.configs")
+
       config.setup({
         ensure_installed = {
           "astro", "dockerfile", "gitignore", "go", "gomod", "gosum",
@@ -13,7 +21,8 @@ return {
         sync_install = false,
         highlight = { enable = true },
         indent = { enable = true, disable = { "yaml" } },
-        auto_install = true
+        auto_install = true,
+        textobjects = { select = { enable = true, lookahead = true } }
       })
     end
   }
