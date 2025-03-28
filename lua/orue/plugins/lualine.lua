411f7c318f5e0e71035c7cf37862956143f67da1
Carlos Orue <carlos@orue.io>
2025-03-28 00:41:35 -0500

    reverse lualine to default

--- lua/orue/plugins/lualine.lua
+++ lua/orue/plugins/lualine.lua
@@ -1,28 +1,7 @@
 return {
   "nvim-lualine/lualine.nvim",
-  dependencies = {
-    "rebelot/kanagawa.nvim",
-    "nvim-tree/nvim-web-devicons",
-  },
+  dependencies = {"rebelot/kanagawa.nvim", "nvim-tree/nvim-web-devicons"},
   config = function()
-    require("lualine").setup({
-      options = {
-	theme = "kanagawa",
-	section_separators = { left = "", right = "" },
-      },
-      sections = {
-	lualine_a = {
-	  { "mode", separator = { left = "" }, right_padding = 2 },
-	},
-	lualine_b = { "branch","diff", "diagnostics"  },
-	lualine_c = {{ "filename", path = 1 } },
-	lualine_x = {"fileformat", "filetype"},
-	lualine_y = { "progress" },
-	lualine_z = {
-	  { "location", separator = { right = "" }, left_padding = 2 },
-	},
-
-      },
-    })
+    require("lualine").setup({options = {theme = "kanagawa" }})
   end
 }
